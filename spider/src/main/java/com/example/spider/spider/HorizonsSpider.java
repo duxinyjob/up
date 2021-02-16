05200397cce0594df2b84a02af92d584ad0f1e42
duxinyjob <duxinyjob@163.com>
2021-02-16 21:03:09 +0800

    Horizons 普通消息抓取

--- spider/src/main/java/com/example/spider/spider/HorizonsSpider.java
+++ spider/src/main/java/com/example/spider/spider/HorizonsSpider.java
@@ -9,9 +9,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
+import java.util.Optional;
 
 @Component
 @Slf4j
@@ -33,8 +35,8 @@ public class HorizonsSpider {
     public void start(){
         log.info("定时任务开启");
         Horizons horizons  = horizonsDao.queryLastDisplayTime();
-        this.getHorizonsJson(pageNo,horizons.getId());
-
+//        this.getHorizonsJson(pageNo,horizons.getId());
+        this.getNowHorizonsGobleJson();
         log.info("定时任务结束");
     }
 
@@ -58,6 +60,34 @@ public class HorizonsSpider {
         if (pageNo < MAX_PAGE){
             Integer page = pageNo +1;
             getHorizonsJson(page,id);
+        }
+    }
+
+    private void getNowHorizonsGobleJson(){
+        Long date = (Long) (System.currentTimeMillis() / 1000);
+        this.getHorizonsGobleJson(date);
+    }
+
+    private void getHorizonsGobleJson(Long pageNo){
+        StringBuilder url = new StringBuilder("https://api.wallstcn.com/apiv1/content/lives?");
+        url.append("cursor=" + pageNo);
+        url.append("&channel=global-channel&client=pc");
+        url.append("&limit=" + limit);
+        url.append("&first_page=false&accept=live%2Cvip-live");
+        String result = restTemplate.getForObject(url.toString(),String.class);
+        log.info("each request" + result);
+        HorizonsDto horizonsDto = JSON.parseObject(result, HorizonsDto.class);
+
+        List<Horizons> horizonsList = HorizonsItemsDto2HorizonsConverage.INSTANCE.jianwenFromJianwenItemsDto(horizonsDto.getData().getItems());
+        for (Horizons horizons : horizonsList) {
+            log.info("insert cell" + horizons);
+            horizonsDao.insert(horizons);
+        }
+
+        if (!CollectionUtils.isEmpty(horizonsList)){
+            Horizons last = horizonsList.get(horizonsList.size() - 1);
+            Optional<Horizons> optional = Optional.ofNullable(last);
+            optional.ifPresent(dto->this.getHorizonsGobleJson(dto.getDisplayTime().getTime() / 1000));
         }
     }
 }
